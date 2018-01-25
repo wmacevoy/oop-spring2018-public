57020db29600b9b761d4539758c3a8e89f2430db
Warren MacEvoy <wmacevoy@gmail.com>
2018-01-25 13:51:06 -0700

    java example

--- powerstrip-java/src/main/java/powerstrip/PowerStrip.java
+++ powerstrip-java/src/main/java/powerstrip/PowerStrip.java
@@ -10,17 +10,56 @@ package powerstrip;
  * @author wmacevoy
  */
 public class PowerStrip {
+    
+    public static final int MODE_ON = 0;
+    
+    private int outlets;
+    private OutletState [] outletStates;
+    private Mode mode;
+    
 
-    public PowerStrip(int outlets) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public PowerStrip(int _outlets) {
+        if (_outlets <= 0) {
+            throw new IllegalArgumentException("outlets (" + _outlets + ") must be positive.");
+        }
+        outlets=_outlets;
+        outletStates = new OutletState[outlets];
+        for (int outlet = 0; outlet < outlets; ++outlet) {
+            outletStates[outlet] = OutletState.UNPOWERED;
+        }
     }
-    public int getOutlets() { return 0; }   
+    public int getOutlets() { return outlets; }   
 
-    public void setModeOff() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setMode(Mode _mode) {
+        mode = _mode;
     }
+    
+    public Mode getMode() {
+        return mode;
+    }
+    
+    public void outletOk(int outlet) {
+        if (outlet < 0 || outlet >= outlets) {
+            throw new IllegalArgumentException("outlet (" + outlet + ") must be between 0 and " + (outlets-1) + ".");
+        }
+    }
+    public void setOutletState(int outlet, OutletState state) {
+        outletOk(outlet);
+        outletStates[outlet]=state;
+    }
+    
+    public OutletState getOutletState(int outlet) {
+        outletOk(outlet);
+        switch(mode) {
+            case OFF: return OutletState.UNPOWERED;
+            case ON: return OutletState.POWERED;
+            case REMOTE: return outletStates[outlet];
+        }
+        throw new IllegalStateException();
+    }
+    
+    public void setModeOff() { setMode(Mode.OFF); }
+    public void setModeOn() { setMode(Mode.ON); }
+    public void setModeRemote() { setMode(Mode.REMOTE); }
 
-    public boolean getOutletState(int outlet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
--- powerstrip-java/src/test/java/powerstrip/PowerStripTest.java
+++ powerstrip-java/src/test/java/powerstrip/PowerStripTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
  * @author wmacevoy
  */
 public class PowerStripTest {
-    
+
     public PowerStripTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -48,16 +48,66 @@ public class PowerStripTest {
         int result = instance.getOutlets();
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void testOutletSafety() {
         PowerStrip instance = new PowerStrip(2);
         instance.setModeOff();
         for (int outlet = 0; outlet < instance.getOutlets(); ++outlet) {
-            boolean expResult = false;
-            boolean result = instance.getOutletState(outlet);
+            OutletState expResult = OutletState.UNPOWERED;
+            OutletState result = instance.getOutletState(outlet);
             assertEquals(expResult, result);
         }
     }
-    
+
+    @Test
+    public void testAllOutlets() {
+        PowerStrip instance = new PowerStrip(2);
+        for (int outlet = 0; outlet < instance.getOutlets(); ++outlet) {
+            testOutletOff(instance, outlet);
+            testOutletOn(instance, outlet);
+            testOutletRemote(instance, outlet);
+        }
+    }
+
+    public void testOutletOff(PowerStrip unit, int outlet) {
+        unit.setModeOff();
+        OutletState expResult = OutletState.UNPOWERED;
+        OutletState result = unit.getOutletState(outlet);
+        assertEquals(expResult, result);
+
+    }
+
+    public void testOutletOn(PowerStrip unit, int outlet) {
+        unit.setModeOn();
+        OutletState expResult = OutletState.POWERED;
+        OutletState result = unit.getOutletState(outlet);
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testNullPointerRegression() {
+        PowerStrip unit = new PowerStrip (2);
+        int outlet = 0;
+        unit.setModeRemote();
+        unit.setOutletState(outlet, OutletState.POWERED);
+        
+    }
+    public void testOutletRemote(PowerStrip unit, int outlet) {
+        {
+            unit.setModeRemote();
+            unit.setOutletState(outlet, OutletState.POWERED);
+            OutletState expResult = OutletState.POWERED;
+            OutletState result = unit.getOutletState(outlet);
+            assertEquals(expResult, result);
+        }
+        {
+            unit.setOutletState(outlet, OutletState.UNPOWERED);
+            OutletState expResult = OutletState.UNPOWERED;
+            OutletState result = unit.getOutletState(outlet);
+            assertEquals(expResult, result);
+        }
+    }
+
 }
