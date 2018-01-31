58cec93af272be19c6e05848a239c6161d5d101e
Warren MacEvoy <wmacevoy@gmail.com>
2018-01-31 00:33:48 -0700

    timer

---
+++ powerstrip-py3/TestTimer.py
@@ -0,0 +1,94 @@
+import unittest
+import datetime
+from Timer import Timer
+
+class TestTimer(unittest.TestCase):
+    def testSetTimeOn(self):
+        timer = Timer()
+        t = datetime.time(hour=22,minute=30,second=15,microsecond=250)
+        timer.setTimeOn(t)
+        s = timer.getTimeOn()
+        self.assertEqual(t,s)
+
+    def testSetTimeOff(self):
+        timer = Timer()
+        t = datetime.time(hour=22,minute=30,second=15,microsecond=250)
+        timer.setTimeOff(t)
+        s = timer.getTimeOff()
+        self.assertEqual(t,s)
+
+    def testTimerTime(self):
+        timer = Timer()
+        jan2010_4pm = datetime.datetime(
+            year=2010,month=1,day=1,hour=16,minute=0,second=0)
+        jan2010_mid = datetime.datetime(
+            year=2010,month=1,day=1,hour=0,minute=0,second=0)
+        timer.setDatetime(jan2010_4pm)
+        now=timer.getDatetime()
+        delta=(now-jan2010_4pm).total_seconds()
+        self.assertLess(abs(delta),1)
+
+        today =timer.getToday()
+        delta=(today-jan2010_mid).total_seconds()
+        self.assertLess(abs(delta),1)
+
+    def testTimerAlwaysOn(self):        
+        timer = Timer()
+        timer.setTimeOn(datetime.time.min)
+        timer.setTimeOff(datetime.time.max)
+        jan2010_mid = datetime.datetime(
+            year=2010,month=1,day=1,hour=0,minute=0,second=0)
+        for m in range(24*60+1):
+            timer.setDatetime(jan2010_mid + datetime.timedelta(seconds=60*m))
+            self.assertTrue(timer.isActive())
+
+    def testTimerAlwaysOff(self):
+        timer = Timer()
+        timer.setTimeOn(datetime.time.min)
+        timer.setTimeOff(datetime.time.min)
+        jan2010_mid = datetime.datetime(
+            year=2010,month=1,day=1,hour=0,minute=0,second=0)
+        for m in range(24*60+1):
+            timer.setDatetime(jan2010_mid + datetime.timedelta(seconds=60*m))
+            self.assertFalse(timer.isActive())
+            
+    def testTimer10pmTil1am(self):
+        timer = Timer()
+        jan2010midnight = datetime.datetime(
+            year=2010,month=1,day=1,hour=0,minute=0,second=0)
+        timer.setDatetime(jan2010midnight)
+        tenPm = datetime.time(hour=22,minute=0,second=0,microsecond=0)
+        timer.setTimeOn(tenPm)
+        oneAm = datetime.time(hour=1,minute=0,second=0,microsecond=0)
+        timer.setTimeOff(oneAm)
+        self.assertTrue(timer.isActive())
+        
+        jan2010_2am = datetime.datetime(2010,1,1,2,0,0)        
+        timer.setDatetime(jan2010_2am)
+        self.assertFalse(timer.isActive())
+
+        dec2009_9pm = datetime.datetime(2009,12,31,21,0,0)        
+        timer.setDatetime(dec2009_9pm)        
+        self.assertFalse(timer.isActive())
+
+    def testTimer10amTil1pm(self):
+        timer = Timer()
+        timer.debug = True
+        jan2010midnight = datetime.datetime(2010,1,1,0,0,0)
+        timer.setDatetime(jan2010midnight)
+        tenAm = datetime.time(10,0,0,0)
+        timer.setTimeOn(tenAm)
+        onePm = datetime.time(13,0,0,0)
+        timer.setTimeOff(onePm)
+        self.assertFalse(timer.isActive())
+        
+        jan2010_2am = datetime.datetime(2010,1,1,2,0,0)        
+        timer.setDatetime(jan2010_2am)
+        self.assertFalse(timer.isActive())
+
+        dec2009_11am = datetime.datetime(2009,12,31,11,0,0)        
+        timer.setDatetime(dec2009_11am)
+        self.assertTrue(timer.isActive())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ powerstrip-py3/Timer.py
@@ -0,0 +1,79 @@
+import datetime
+
+class Timer:
+    def __init__(self):
+        self._delta = datetime.timedelta()
+        self._on = Timer.timeToTimedelta(datetime.time.min)
+        self._off = Timer.timeToTimedelta(datetime.time.max)
+        
+    def setDatetime(self, now):
+        if not isinstance(now,datetime.datetime):
+            raise ValueError("now must be a datetime")
+        self._delta = now - datetime.datetime.utcnow()
+
+    def getDatetime(self):
+        return datetime.datetime.utcnow() + self._delta
+
+    def timeToTimedelta(time):
+        if not isinstance(time,datetime.time):
+            raise ValueError("time must be a time")
+        return datetime.timedelta(
+            days=0,
+            seconds=time.second,
+            microseconds=time.microsecond,
+            milliseconds=0,
+            minutes=time.minute,
+            hours=time.hour)
+
+
+    def timedeltaToTime(timedelta):
+        if not isinstance(timedelta,datetime.timedelta):
+            raise ValueError("timedelta must be a timedelta")
+        tdHour=timedelta.days*24+timedelta.seconds//(60*60)
+        tdMinute=(timedelta.seconds//60) % 60
+        tdSecond=timedelta.seconds % 60
+        tdMicros=timedelta.microseconds
+        return datetime.time(
+            hour=tdHour,
+            minute=tdMinute,
+            second=tdSecond,
+            microsecond=tdMicros)
+
+    def setTimeOff(self, off):
+        self._off = Timer.timeToTimedelta(off)
+
+    def getTimeOff(self):
+        return Timer.timedeltaToTime(self._off)
+
+    def setTimeOn(self, on):
+        self._on = Timer.timeToTimedelta(on)
+
+    def getTimeOn(self):
+        return Timer.timedeltaToTime(self._on)
+
+    def getToday(self,value=None):
+        if value != None and not isinstance(value,datetime.datetime):
+            raise ValueError("value must be None or a datetime")
+        if value == None: value = self.getDatetime()
+        return datetime.datetime(
+            year=value.year,
+            month=value.month,
+            day=value.day,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=value.tzinfo)
+
+    def isActive(self):
+        now = self.getDatetime()
+        today = self.getToday(now)
+        todayOn = today + self._on
+        todayOff = today + self._off
+        yesterday = today - datetime.timedelta(days=1)
+        yesterdayOn = yesterday + self._on
+        yesterdayOff = yesterday + self._off
+
+        if yesterdayOn > yesterdayOff and todayOff > now: return True
+        if todayOn <= now and todayOff > now: return True
+        return False
